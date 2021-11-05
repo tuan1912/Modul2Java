bd191d8e53989efd8b8f358f24d7337ecc63ff16
tuan1912 <tuan.nguyen191292@gmail.com>
2021-11-05 16:35:38 +0700

    5/11

---
+++ lesson3-array-method/src/lesson1/DeleteElement.java
@@ -0,0 +1,38 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class DeleteElement {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a value:");
+        int inputValue = scanner.nextInt();
+        deleteElement(inputValue, array);
+
+    }
+
+    public static int[] deleteElement(int value, int[] arr) {
+        boolean check = false;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]) {
+                check = true;
+                index = i;
+                break;
+            }
+        }
+        if (check) {
+            for (int i = index; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length - 1] = 0;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+}
